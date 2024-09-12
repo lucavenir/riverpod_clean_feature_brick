371729239b2f589623e2478f887546be692c2aa2
~venir <luca@venir.dev>
2024-09-12 17:07:00 +0200

    v 0.0.4
    bug fixes

--- brick/__brick__/lib/src/{{name.snakeCase()}}/models/{{name.snakeCase()}}.model.dart
+++ brick/__brick__/lib/src/{{name.snakeCase()}}/models/{{name.snakeCase()}}.model.dart
@@ -1,6 +1,6 @@
 {{#freezed}}
 import 'package:freezed_annotation/freezed_annotation.dart';
-part '{{name.snakeCase()}}.freezed.dart';
+part '{{name.snakeCase()}}.model.freezed.dart';
 @freezed
 class {{name.pascalCase()}} with _${{name.pascalCase()}} {
   const factory {{name.pascalCase()}}(
